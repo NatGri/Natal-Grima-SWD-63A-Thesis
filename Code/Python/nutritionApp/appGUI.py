0f06f743d175c6930b1c0841706c4a5ed605a306
Natal-Grima <86468775+NatGri@users.noreply.github.com>
2023-05-29 14:33:03 +0200

    videoCapture bugfix

--- Code/Python/nutritionApp/appGUI.py
+++ Code/Python/nutritionApp/appGUI.py
@@ -53,7 +53,7 @@ class Nutrition:
 
         img_lbl = tk.Label(camera_frame, border=0, background='WHITE', width=widgets_width,
                            height=int(self.__win_height / 1.5))
-        cap = cv2.VideoCapture(1)
+        cap = cv2.VideoCapture(0)
         cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
         cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
